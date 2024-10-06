79a2e997f2d19555f7cba4c2b03b55d114ab02fd
Lea Delic <leadelic0429@gmail.com>
2024-10-06 13:21:38 -0400

    Create Employee class with properties and methods

---
+++ employee_management.js
@@ -0,0 +1,13 @@
+//task 1
+class Employee (name, salary, position, department) {
+    getDetails(
+        name= Fatima,
+        salary= 80000,
+        position= Developer,
+        department= Engineering,
+    )
+};
+
+
+
+
